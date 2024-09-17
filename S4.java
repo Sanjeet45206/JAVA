cffcf895a261457c51099cc61e0e72ec79015f7b
Sanjeet45206 <sanjeet.23cs022@sode-edu.in>
2024-09-17 15:45:51 +0530

    Add files via upload

---
+++ S4.java
@@ -0,0 +1,48 @@
+package pkg1;
+class Shape{
+    public void draw(){
+        System.out.println("drawing a shape");
+    }
+    public void erase() {
+        System.out.println("Erasing a shape");
+    }
+}
+class Circle extends Shape{
+    public void draw(){
+        System.out.println("drawing a Circle");
+    }
+    public void erase() {
+        System.out.println("Erasing a Circle");
+    }
+}
+class Triangle extends Shape{
+    public void draw(){
+        System.out.println("drawing a triangle");
+    }
+    public void erase() {
+        System.out.println("Erasing a triangle");
+    }
+}
+class Square extends Shape{
+    public void draw(){
+        System.out.println("drawing a square");
+    }
+    public void erase() {
+        System.out.println("Erasing a square");
+    }
+}
+public class S4 {
+    public static void main(String[] args) {
+        Shape[] shapes=new Shape[3];
+        shapes[0]=new Circle();
+        shapes[1]=new Triangle();
+        shapes[2]=new Square();
+        for(int i=0;i<shapes.length;i++){
+            shapes[i].draw();
+            shapes[i].erase();
+            System.out.println("----------");
+         
+        }
+    }
+    
+}
