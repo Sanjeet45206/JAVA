40743e23359f3ac187d2d926d49481c073b1cc7d
Sanjeet45206 <sanjeet.23cs022@sode-edu.in>
2024-11-05 15:54:23 +0530

    Add files via upload

---
+++ M7.java
@@ -0,0 +1,9 @@
+package pkg1;
+import pkg1.Tools1;
+public class M7 {
+    public static void main(String[] args) {
+        Tools1 t1=new Tools1();
+        System.out.println(t1.reverse("Sanjeet"));
+    }
+    
+}
