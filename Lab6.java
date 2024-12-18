e5901634f6fb2a4004dbf63d1d776fd1b5dbe2c7
Sanjeet45206 <sanjeet.23cs022@sode-edu.in>
2024-12-19 00:40:34 +0530

    Update and rename S5.java to Lab6.java

--- Lab6.java
+++ Lab6.java
@@ -24,7 +24,7 @@ class Triangle1 extends Shape1{
     double calculatePerimeter() {
         return side1+side2+side3;
     }}
-public class S5 {
+public class Lab6 {
     public static void main(String[] args){
         Circle1 circle1=new Circle1(5);
         System.out.println("Area and Circumference of Circle are");
