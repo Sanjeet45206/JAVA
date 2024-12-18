ccdea1bd72d2c67917a7583eaa5592eff41517bc
Sanjeet45206 <sanjeet.23cs022@sode-edu.in>
2024-12-18 15:30:57 +0530

    Update and rename S4.java to Lab5.java

--- Lab5.java
+++ Lab5.java
@@ -31,7 +31,7 @@ class Square extends Shape{
         System.out.println("Erasing a square");
     }
 }
-public class S4 {
+public class Lab5 {
     public static void main(String[] args) {
         Shape[] shapes=new Shape[3];
         shapes[0]=new Circle();
